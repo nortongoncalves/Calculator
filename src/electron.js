8748a2b7b62bf4352d4f035fb67b9cd42a892da0
nortongoncalves <goncalvespnorton@gmail.com>
2020-02-20 00:27:00 -0300

    adicionando arquivo do electron

---
+++ src/electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron')
+const { resolve } = require('path');
+function createWindow () {
+  // Cria uma janela de navegação.
+  const win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  // and load the index.html of the app.
+  win.loadFile(resolve(__dirname, '../build', 'index.html'))
+
+  // Open the DevTools.
+  //win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Algumas APIs podem ser usadas somente depois que este evento ocorre.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // No macOS é comum para aplicativos e sua barra de menu 
+  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
